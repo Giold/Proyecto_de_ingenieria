c0da83996941d25e31e382638722f9ef252b6bf3
Giold <64499571+Giold@users.noreply.github.com>
2022-08-29 01:21:34 -0500

    prueba

---
+++ CS13309_Archivos_HTML/FileReader/src/App.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Time;
+
+public class App {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("/Users/USER/Desktop/CS13309_Archivos_HTML/FileReader/Files/002.html");
+
+        System.out.println();
+
+    }
+}
