ca675fdf305657dbf201907fca2a63f162e084f5
leonardo.gonzalez <leonardo.gonzalez@catuxy.com>
2022-09-29 12:12:26 -0500

    Se cambio el algoritmo de conteo para la actividad 6 y se agregaron las carpetas con los resultados

--- Actividad5ArchivoDuplicado/Codigo/ProyectoV2.py
+++ Actividad5ArchivoDuplicado/Codigo/ProyectoV2.py
@@ -1,3 +1,4 @@
+from encodings import utf_8
 import sys
 import os
 import re
@@ -5,10 +6,10 @@ import time # libreria para cronometrar
 from bs4 import BeautifulSoup # libreria para eliminar tags html - (pip install beautifulsoup4)
 from prettytable import PrettyTable
 
-archivos = "Files" # Carpeta de archivos html
-archivosfix = "FilesFix" # Carpeta para almacenar archivos html sin etiquetas
-archivosList = "FilesList" # Carpeta para almacenar lista de texto de los html
-archivosToken = "FilesToken" # Carpeta para almacenar listas tokenizadas
+archivos = r"C:\Users\vatil\Documents\Tarea\Agosto-Dec 2022\Proyectos de Ingenieria de software\fase2\Proyecto_de_ingenieria\Actividad5ArchivoDuplicado\Files" # Carpeta de archivos html
+archivosfix = r"C:\Users\vatil\Documents\Tarea\Agosto-Dec 2022\Proyectos de Ingenieria de software\fase2\Proyecto_de_ingenieria\Actividad5ArchivoDuplicado\Codigo\FilesFix" # Carpeta para almacenar archivos html sin etiquetas
+archivosList = r"C:\Users\vatil\Documents\Tarea\Agosto-Dec 2022\Proyectos de Ingenieria de software\fase2\Proyecto_de_ingenieria\Actividad5ArchivoDuplicado\Codigo\FilesList" # Carpeta para almacenar lista de texto de los html
+archivosToken = r"C:\Users\vatil\Documents\Tarea\Agosto-Dec 2022\Proyectos de Ingenieria de software\fase2\Proyecto_de_ingenieria\Actividad5ArchivoDuplicado\Codigo\FilesToken" # Carpeta para almacenar listas tokenizadas
 
 #Funcion para abrir y cronometrar los archivos 
 def open_file():
@@ -20,7 +21,7 @@ def open_file():
 
     for filename in os.listdir(archivos):
         start = time.time()
-        with open(os.path.join(archivos, filename), "r") as f:
+        with open(os.path.join(archivos, filename), "r",  errors= "ignore") as f:
             end = time.time()
             total+= (end - start)
 
@@ -42,7 +43,7 @@ def remove_html_tags():
 
     for filename in os.listdir(archivos):
         start = time.time()
-        with open(os.path.join(archivos, filename), "r") as f:
+        with open(os.path.join(archivos, filename), "r",  errors= "ignore") as f:
 
             with open(os.path.join(archivosfix, filename), 'w') as outfile: 
                 soup = BeautifulSoup(f,'html.parser').text # Remover tags de html
@@ -69,12 +70,13 @@ def create_list():
 
     for filename in os.listdir(archivos):
         start = time.time()
-        with open(os.path.join(archivosfix, filename), "r") as f:
+        with open(os.path.join(archivosfix, filename), "r",  ) as f:
                       
             with open(os.path.join(archivosList, filename), 'w+') as outfile:
                     for lines in f:
                         line = re.sub(r"(\d+)", '', lines) # Remover numeros
-                        line = re.sub(r"[^\w\s]","", line) # Remover caracteres especiales
+                        line = re.sub(r"[^\w\s]","", line) # Remover caracteres especiales (r"[^\w\s]","", line)
+                        #line = re.sub(r"(\s)","", line) # Remover caracteres especiales (r"[^\w\s]","", line)
                         line = re.sub(r"\n","", line)# Remover saltos de linea 
                         line = re.sub(r"_","", line) # Remover guiones bajos
                         line = line.lower().split()# Divison de texto y poner en minusculas
@@ -142,7 +144,7 @@ def Ascii():
     countTotalAr = dict()
     countTotal = dict()
 
-    mytable = PrettyTable(["Token","Repeticiones","# de archivos con ese token"])
+    mytable = PrettyTable(["Token","Repeticiones","# de archivos con ese token"]) #,"# de archivos con ese token"
 
     sys.stdout = open("A6_matricula.txt", "w+") #creacion de txt con los tiempos
 
@@ -150,23 +152,27 @@ def Ascii():
         start = time.time()
         with open(os.path.join(archivosToken, filename), "r") as f:
                 
-            with open(os.path.join(archivosList, filename), 'r') as outfile:
-                    for word in f: # Saber la cantidad de archivos que contienen una misma palabra 
-                        word = re.sub(r"(\d+)", '', word)#SEGUNDO NUMERO ES EL VALOR
+            #with open(os.path.join(archivosList, filename), 'r') as outfile:
+                    for word in f: # Saber la cantidad de archivos que contienen una misma palabra                      
                         word = re.sub(r"\n","", word)
-
+                        number=(re.sub(r"\s","",word))#Quitar el espacio en blanco
+                        number=int(re.sub(r"(\D+)","",word))#Quitar los no numeros
+                        word = re.sub(r"(\d+)", '', word)#Quita los numeros                    
+                        
                         if word in countTotalAr:
-                            countTotalAr[word]+=1      
+                            countTotalAr[word]+=number
+                            countTotal[word]+=1
                         else:
-                            countTotalAr[word] = 1
+                            countTotalAr[word] = number
+                            countTotal[word]=1
                     
-                    for word in outfile:
-                        word = re.sub(r"\n","", word) #Eliminar saltos de linea
+                    #for word in outfile:                       
+                        #word = re.sub(r"\n","", word) #Eliminar saltos de linea
 
-                        if word in countTotal: #Si la palabra esta en el conteo suma
-                            countTotal[word]+=1 
-                        else:             #Si no, es la primera vez que aparece la palabra
-                            countTotal[word] = 1
+                        #if word in countTotal: #Si la palabra esta en el conteo suma
+                            #countTotal[word]+=1 
+                        #else:             #Si no, es la primera vez que aparece la palabra
+                            #countTotal[word] = 1
 
                     end = time.time()
                     total+= (end - start)
@@ -174,17 +180,20 @@ def Ascii():
         print("Proyecto_IDS/files/"+filename, "\t", end - start)
         endF = time.perf_counter()
 
-    countTotal.update(countTotalAr)
-    with open("Token_List.txt","w") as fin:
-        for key, value in countTotal.items():
-            value = str(value)
-            fin.write(key+" "+value+"\n")
-
-        with open("Token_List.txt","w") as fin:
-            for key, value in countTotalAr.items():
-                value = str(value)
-                value = "\n".join(value)
-                fin.write(value)
+    #countTotal.update(countTotalAr)
+    with open("Token_List.txt","w") as fin:   
+        for key, value in countTotalAr.items():             
+            value = str(value)           
+            value_2= str(countTotal.get(key))
+            #fin.write(key+", "+value+", "+value_2+"\n")            
+            mytable.add_row([key,value,value_2])        
+        stingtable= mytable.get_string()
+        fin.write(stingtable)
+    #with open("Token_List.txt","w") as fin:
+    #        for key, value in countTotalAr.items():
+    #            value = str(value)
+     #           value = "\n".join(value)
+      #          fin.write(value)   
 
     print("\ntiempo total en acumular el diccionario:", f"{total:.2f}", "segundos")
     print("\ntiempo total de ejecucion:", f"{endF-startF:.2f}", "segundos")
@@ -198,3 +207,4 @@ open_file()
 remove_html_tags()
 create_list()
 tokenization()
+Ascii()
