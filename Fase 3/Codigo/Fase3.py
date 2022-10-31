22e1c0e7e2cdd5846286782cf9ded26bc4a57c3b
Giold <64499571+Giold@users.noreply.github.com>
2022-10-31 10:19:10 -0600

    Update

--- Fase 3/Codigo/Fase3.py
+++ Fase 3/Codigo/Fase3.py
@@ -6,10 +6,10 @@ import time # libreria para cronometrar
 from bs4 import BeautifulSoup # libreria para eliminar tags html - (pip install beautifulsoup4)
 from prettytable import PrettyTable
 
-archivos = r"C:\Users\USER\Documents\GitHub\Proyecto_de_ingenieria\Actividad5ArchivoDuplicado\Files" # Carpeta de archivos html
-archivosfix = r"C:\Users\USER\Documents\GitHub\Proyecto_de_ingenieria\Actividad5ArchivoDuplicado\Codigo\FilesFix" # Carpeta para almacenar archivos html sin etiquetas
-archivosList = r"C:\Users\USER\Documents\GitHub\Proyecto_de_ingenieria\Actividad5ArchivoDuplicado\Codigo\FilesList" # Carpeta para almacenar lista de texto de los html
-archivosToken = r"C:\Users\USER\Documents\GitHub\Proyecto_de_ingenieria\Actividad5ArchivoDuplicado\Codigo\FilesToken" # Carpeta para almacenar listas tokenizadas
+archivos = r"C:\Users\USER\Documents\GitHub\Proyecto_de_ingenieria\Fase3\Files" # Carpeta de archivos html
+archivosfix = r"C:\Users\USER\Documents\GitHub\Proyecto_de_ingenieria\Fase3\Codigo\FilesFix" # Carpeta para almacenar archivos html sin etiquetas
+archivosList = r"C:\Users\USER\Documents\GitHub\Proyecto_de_ingenieria\Fase3\Codigo\FilesList" # Carpeta para almacenar lista de texto de los html
+archivosToken = r"C:\Users\USER\Documents\GitHub\Proyecto_de_ingenieria\Fase3\Codigo\FilesToken" # Carpeta para almacenar listas tokenizadas
 
 
 
